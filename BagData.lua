3a4b0b47fc51a3414dd9f3c0d676ed609a77bb37
Jeff Jacob <Jeff Jacob>
2016-11-25 21:35:18 -0800

    Still a buggy mess but it's tracking some stuff

---
+++ BagData.lua
@@ -0,0 +1,103 @@
+
+--bagdata
+BagData = {} 
+
+function BagData:new (o, console)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    self.console = console
+    if (not self.changes) then
+        self.changes = {}
+    end
+    return o
+end
+
+
+function BagData:Test()
+    self.console:Print("basedata test called")
+end
+
+function BagData:Reset()
+    self.bag = {}
+    self.changes = {}
+end
+
+
+
+function BagData:InitBag(bagId,slotId)
+    if (not self.bag) then
+        self.bag = {}
+    end 
+    if (not self.bag[bagId]) then
+        self.bag[bagId] = {}
+    end
+    if (not self.bag[bagId][slotId]) then
+        self.bag[bagId][slotId] = {}
+    end
+end
+
+function BagData:UpdateItem(bagId,slotId,itemName,itemNumber,itemCount,texture)
+    self.console:Print("Updating item number:"..itemNumber.." to "..itemCount.." in bag "..bagId.." slot "..slotId)
+    self:InitBag(bagId,slotId)
+    if (not self.bag[bagId][slotId]) then
+        self.console:Print("Was "..self.bag[bagId][slotId]["itemNumber"].." to "..self.bag[bagId][slotId]["itemCount"].." in bag "..bagId.." slot "..slotId)
+    else
+        self.console:Print("Was empty in bag "..bagId.." slot "..slotId)
+    end
+    if (self.bag[bagId][slotId]["itemNumber"] ~= itemNumber) then -- item in slot changed
+        local oldItemNumber = self.bag[bagId][slotId]["itemNumber"]
+        if (oldItemNumber ~= nil) then -- slot was not empty before empty it and uncount it's contents first
+            self:SubtractItem(oldItemNumber,self.bag[bagId][slotId]["itemCount"])
+        end -- end empty slot
+        self:AddItem(itemNumber,itemCount)
+    else -- adding more to the stack
+        local diff = itemCount - self.bag[bagId][slotId]["itemCount"]
+        self.console:Print(itemCount.."-"..self.bag[bagId][slotId]["itemCount"].."="..diff)
+        self:AddItem(itemNumber,diff)
+    end -- end changeB
+    --save the new item
+    self.bag[bagId][slotId]["itemNumber"] = itemNumber
+    self.bag[bagId][slotId]["itemCount"] = itemCount
+    self.console:Print("Became "..self.bag[bagId][slotId]["itemNumber"].." to "..self.bag[bagId][slotId]["itemCount"].." in bag "..bagId.." slot "..slotId)
+end -- end UpdateItem
+
+
+function BagData:ClearItem(bagId,slotId)
+    self:InitBag(bagId,slotId)
+    if (self.bag[bagId][slotId] == nil) then
+        return -- already blank
+    end
+    local oldItemNumber = self.bag[bagId][slotId]["itemNumber"]
+    if (oldItemNumber ~= nil) then -- slot was not empty before
+        self.console:Print("removing stack of " .. oldItemNumber)
+        self:SubtractItem(oldItemNumber,self.bag[bagId][slotId]["itemCount"])
+        self.bag[bagId][slotId] = nil;
+        self.console:Print(oldItemNumber.."*"..self.changes[oldItemNumber])
+    end -- end empty slot
+end
+
+function BagData:AddItem(itemNumber,itemCount)
+    self.console:Print("Adding "..itemCount.." of "..itemNumber)
+    self.changes[tostring(itemNumber)] = self:ToZero(self.changes[itemNumber]) + itemCount
+    self.console:Print("AddItem changes: ".. #self.changes)
+end -- SubtractItem
+
+function BagData:SubtractItem(itemNumber,itemCount)
+    self:AddItem(itemNumber,itemCount * -1)
+end -- SubtractItem
+
+function BagData:LogChanges()
+    self.console:Print("LogChanges : ".. #self.changes)
+    for key,value in pairs(self.changes) do
+        self.console:Print(key..":"..value)
+    end
+    self.console:Print("DONE LogChanges")
+end
+
+function BagData:ToZero(value)
+    if (value == nil) then 
+        return 0
+    end
+    return value
+end
--- GatheringTracker.lua
+++ GatheringTracker.lua
@@ -26,6 +26,11 @@ function GatheringTracker:OnInitialize()
     options.args.profile = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
     LibStub("AceConfig-3.0"):RegisterOptionsTable("GatheringTracker", options, {"gt", "GatheringTracker"})
     GatheringTracker:RegisterEvent("BAG_UPDATE")
+    GatheringTracker:RegisterEvent("BAG_UPDATE_DELAYED")
+    --GatheringTracker:RegisterEvent("ITEM_PUSH")
+     if (self.db.char.bagdata == nil or self.db.char.bagdata.Test == nil) then
+        self.db.char.bagdata = BagData:new(self.db.char.bagdata,self)
+    end
 end
 
 function GatheringTracker:OnEnable()
@@ -34,7 +39,6 @@ function GatheringTracker:OnEnable()
     if type(GatheringTracker.db.profile.timeFrame) ~= "number" then
         GatheringTracker.db.profile.timeFrame = 60;
     end -- end if
-
     -- Called when the addon is enabled
 end
 
@@ -51,8 +55,73 @@ function GatheringTracker:SetTimeFrame(info,input)
     self.db.profile.timeFrame = input
 end
 
-function GatheringTracker:BAG_UPDATE(eventName,bagID)
+
+function GatheringTracker:BAG_UPDATE(eventName,bagId)
     self:Print(eventName)
-    self:Print(bagID)
+    --self:Print(bagId)
+    self:ScanBag(bagId)
 end
+
+function GatheringTracker:BAG_UPDATE_DELAYED(eventName)
+    self:Print(eventName)
+    self:FinalizeTracking()
+end
+--[[]
+function GatheringTracker:ITEM_PUSH(eventName,bagId,icon)
+    self:Print(eventName)
+    self:Print(bagId)
+    self:Print("Icon"..  bagId)
+    self:ScanBag(bagId)
+end
+]]
+
+
+
+--return any new items in the form of an array 
+function GatheringTracker:ScanBag(bagId)
+	local numSlots = GetContainerNumSlots(bagId)
+    ret = {}
+   
+	for slotId = 1, numSlots do
+		local texture, itemCount, _, quality, _, _, itemLink = GetContainerItemInfo(bagId, slotId)
+		if itemLink ~= nil then
+            local _,_,itemType, itemNumber, itemName = string.find(itemLink,"|%x*|H([^:]*):(%d*):[^|]*|h%[([^%]]*)")
+			self:Print("-------------------------------------------")
+            --printable = gsub(itemLink, "|", "||");
+            --self:Print(printable)
+            --self:Print("ItemType :"..itemType)
+            --self:Print("Name:"..itemNumber)
+            self:Print(itemLink.." x"..itemCount)
+            --self:Print("texture:"..texture)
+            --self:Print("itemCount:"..itemCount)
+            --self:Print("quality:"..quality)
+            --self:Print("itemLink:"..itemLink)
+            if (itemType == "item") then
+                if (quality >= 1) then-- ignore poor items (trash)
+                    --check the current bag item
+                    --self:Print("Calling Bag update")
+                    --self.db.char.bagdata:Test()
+                    
+                    self.db.char.bagdata:UpdateItem(bagId,slotId,itemName,itemNumber,itemCount,texture)
+                    --UpdateItem(bagId,slotId,itemName,itemNumber,itemCount,texture)
+                end -- end quality check
+            end -- end item type check
+        else -- blank spot
+            self.db.char.bagdata:ClearItem(bagId,slotId)
+            
+		end --end itemLink check
+	end
+end
+
+
+function GatheringTracker:FinalizeTracking()
+    self:Print("FinalizeTracking")
+    self.db.char.bagdata:LogChanges()
+    self:Print("DONE FinalizeTracking")
+end
+
+
+
+--- helper funtion
+
 
